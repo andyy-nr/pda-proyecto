6581cdca3b1d0741ea24d6c242ba3693b8831a0a
Andrea <andrea.nunez14545@est.uca.edu.ni>
2023-05-01 22:27:59 -0600

    Agregar capa Vistas

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import QtWidgets, QtCore
+from vistas.inicio import Ui_frmInicio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication(sys.argv)
+    frm = QtWidgets.QWidget()
+    ui = Ui_frmInicio()
+    ui.setupUi(frm)
+    frm.showMaximized()
+    sys.exit(app.exec_())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
